9f2ce95bd92d3864b3200ed968f43eb88155dea0
Speelmasters <cspeelman68@gmail.com>
2022-05-28 22:47:32 -0600

    Fixed JS

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -3,12 +3,15 @@ document.querySelector(
 ).textContent = `Last Modification: ${document.lastModified}`;
 document.querySelector('#©').textContent = new Date(Date.now()).getFullYear();
 
-const today = new Date();
-  const day = today.getDay();
+const today = new Date(Date.now());
+
+  const monthday = today.getDate()
+  const weekday = today.getDay();
   const month = today.getMonth();
   const year = today.getFullYear();
   const daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
   const monthlist = [ "January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December" ];
-
-  document.querySelector('#date').textContent = (`${daylist[day]}, ${day} ${monthlist[month]} ${year}`);
-
+  document.getElementById('date').textContent = (`${daylist[weekday]}, ${monthday} ${monthlist[month]} ${year}`);
+if (day > 1) {
+  document.getElementById("BannerID").style.display = "none"
+}
