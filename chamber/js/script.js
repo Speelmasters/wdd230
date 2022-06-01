99b23e036e283c7b0ef1f170bde9a7d87d8b5020
Speelmasters <cspeelman68@gmail.com>
2022-05-31 19:32:56 -0600

    Add Local Storage

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -13,5 +13,54 @@ const today = new Date(Date.now());
   const monthlist = [ "January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December" ];
   document.getElementById('date').textContent = (`${daylist[weekday]}, ${monthday} ${monthlist[month]} ${year}`);
 if (weekday > 1) {
-  document.getElementById("BannerID").style.display = "none"
+  let banner = document.getElementById("BannerID")
+  if (banner) {
+    banner.style.display = "none";
+  }
 }
+
+
+function msToTime(s) {
+  var ms = s % 1000;
+  s = (s - ms) / 1000;
+  var secs = s % 60;
+  s = (s - secs) / 60;
+  var mins = s % 60;
+  var hrs = ((s - mins) / 60) % 24;
+  var days = Math.floor(((s - mins) / 60) / 24);
+
+  return days + '- Days, ' + hrs + '- Hrs, ' + mins + '- Min'; // + secs + '.' + ms;
+}
+
+function daysBetween(s) {
+  var ms = s % 1000;
+  s = (s - ms) / 1000;
+  var secs = s % 60;
+  s = (s - secs) / 60;
+  var mins = s % 60;
+  var hrs = ((s - mins) / 60) % 24;
+  var days = Math.floor(((s - mins) / 60) / 24);
+
+  return days;
+}
+
+const visitsDisplay = document.querySelector(".visit");
+
+let lastVisit = Number(window.localStorage.getItem("lastvisit-ls"));
+
+if (lastVisit !== 0) {
+  //  testday = new Date("5/29/2022")
+	let difference = daysBetween(Date.now() - lastVisit);
+  if (difference > 0){
+    visitsDisplay.textContent = difference;
+  }
+  else {
+  visitsDisplay.textContent = 0;
+  }
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+
+// store the new number of visits value
+localStorage.setItem("lastvisit-ls", Date.now());
